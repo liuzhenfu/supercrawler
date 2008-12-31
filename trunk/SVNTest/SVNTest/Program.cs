5265de690a82ee2e11b0eb920ef082c95be5a5bc
zhangyunqiao <zhangyunqiao@91f36c04-d78f-11dd-b6b4-918b84bbd919>
2008-12-31 23:35:58 +0000

    Google SVN Test

---
+++ trunk/SVNTest/SVNTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVNTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuck you, dick sucker!");
+        }
+    }
+}
